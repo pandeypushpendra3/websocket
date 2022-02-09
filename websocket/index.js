573769098655eadfb3ab4b04e542d577eb831085
pandeypushpendra3 <pandeypushpendra3@gmail.com>
2022-02-10 01:13:01 +0530

    websocket-1 completed

--- websocket/index.js
+++ websocket/index.js
@@ -1,15 +1,32 @@
 const ws = require('ws');
 //.web socket server
 const port = 8000;
-const wss = new ws.WebSocketServer({port:port},()=>{
+const wsserver = new ws.WebSocketServer({port:port},()=>{
     console.log("listening to the port:8000")
 });
 
-wss.on('connection',function connection(ws){
+wsserver.on('connection',function connection(ws){
     // console.log('connection')
+    let count =0;
     ws.on("message",function incoming(message){
-        console.log("recieved: %s",message);
-    });
-    console.log(`a new user has connected`,ws);
-ws.send("something");
+   const payload = JSON.parse(message);
+// loop through all live clients a
+//send everyone the messafe//
+wsserver.clients.forEach(client=>{
+    if(client.readyState === ws.OPEN){
+        payload.type ="user_message"
+     payload.timestamp = new Date().toUTCString();
+        client.send(JSON.stringify(payload));
+    }
 })
+   console.log(payload);
+   console.log("recieved: %s",message);
+    });
+    console.log(`a new user has connected`);
+ws.send("conncected successfully");
+
+// setInterval(()=>{
+//     ws.send(`${++count} seconds are over`);
+// },1000)
+
+});
